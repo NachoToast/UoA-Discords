bbb5a4176cde5b07667fa0c78a05b1e086b687f2
NachoToast <32235595+NachoToast@users.noreply.github.com>
2024-05-17 17:21:13 +1200

    fix api spec import

--- server/src/loaders/loadApiSpec.ts
+++ server/src/loaders/loadApiSpec.ts
@@ -1,18 +1,20 @@
 import express from 'express';
-import { join } from 'path';
+import { readFileSync } from 'fs';
 import swaggerUi from 'swagger-ui-express';
 import { CONFIG } from '../config/index.js';
-import apiSpec from '../openapi.json';
 
 export function loadApiSpec(): void {
+    // ESM JSON imports are experimental, so for now we'll use this method to
+    // import the API spec.
+    const apiSpec = JSON.parse(
+        readFileSync('openapi.json', 'utf-8'),
+    ) as swaggerUi.JsonObject;
+
     CONFIG.app.use(
         '/api-docs',
         swaggerUi.serve,
         swaggerUi.setup(apiSpec, { customSiteTitle: 'UoA Discords API' }),
     );
 
-    CONFIG.app.use(
-        '/spec',
-        express.static(join(import.meta.dirname, '../', 'openapi.json')),
-    );
+    CONFIG.app.use('/spec', express.static('openapi.json'));
 }
